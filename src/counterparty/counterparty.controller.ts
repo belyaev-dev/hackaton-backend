c12418cd8f6683d0fe091dfb3ab04def373164a0
Denis Belyaev <d.belyaev@nwamo.ru>
2023-02-18 16:15:36 +0300

    upd env

--- src/counterparty/counterparty.controller.ts
+++ src/counterparty/counterparty.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Logger } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CounterpartyService } from './counterparty.service';
-
+@ApiTags('counterparty')
 @Controller('counterparty')
 export class CounterpartyController {
   private logger = new Logger(CounterpartyController.name);
