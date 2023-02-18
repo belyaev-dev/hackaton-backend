e12c11fdd0b9538a08e752c895b8cf946d32adac
Denis Belyaev <d.belyaev@nwamo.ru>
2023-02-18 15:36:22 +0300

    add swagger (wip)

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Logger } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CompanyService } from './company.service';
 
+@ApiTags('company')
 @Controller('company')
 export class CompanyController {
   private logger = new Logger(CompanyController.name);
