c2ef8ea49d604d04926301d656a8c88d34b51b52
Denis Belyaev <d.belyaev@nwamo.ru>
2023-02-18 16:15:37 +0300

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,8 @@ module.exports = {
         watch: false,
         env: {
             ENVIRONMENT: "prod",
+            DATABASE_URL: "postgres://postgres:Z3vBnzalNJ02m3Hd@db.trahswbinhowuhrcwezf.supabase.co:6543/postgres?pgbouncer=true&connection_limit=1",
+            SHADOW_DATABASE_URL: "postgres://postgres:Z3vBnzalNJ02m3Hd@db.trahswbinhowuhrcwezf.supabase.co:5432/postgres"
         }
     }]
 }
