7dca4550dbac30ce2673dbe867f59ae98f69d216
Denis Belyaev <d.belyaev@nwamo.ru>
2023-02-18 15:36:14 +0300

    add crud

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -8,9 +8,23 @@ export class CompanyService {
 
   constructor(private readonly prisma: PrismaService) {}
 
-  public async getCompanyById(id: number): Promise<Company | null> {
-    return this.prisma.company.findUnique({
-      where: { id },
-    });
+  async create(data: any): Promise<Company> {
+    return this.prisma.company.create({ data });
+  }
+
+  async findAll(): Promise<Company[]> {
+    return this.prisma.company.findMany({});
+  }
+
+  async findOne(id: number): Promise<Company | null> {
+    return this.prisma.company.findUnique({ where: { id } });
+  }
+
+  async update(id: number, data: any): Promise<Company | null> {
+    return this.prisma.company.update({ where: { id }, data });
+  }
+
+  remove(id: number): void {
+    this.prisma.company.delete({ where: { id } });
   }
 }
