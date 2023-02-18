f6f3bda353087f238d37e5c56aba40d8d72640d2
Denis Belyaev <d.belyaev@nwamo.ru>
2023-02-18 05:58:59 +0300

    - add cors support - add swagger support - enable validationpipe - add helmet to defend API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // enable cors
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  // Request Validation
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  app.use(helmet());
+
+  // Swagger API Documentation
+  const options = new DocumentBuilder()
+    .setTitle('Leaseasy // hackathon')
+    .setDescription('API сервиса Leaseasy')
+    .setVersion('0.1.0')
+    // .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
