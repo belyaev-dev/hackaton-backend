6a9c9e012bc6573d9cb1f95da267ce0e888bd97a
Denis Belyaev <d.belyaev@nwamo.ru>
2023-02-18 13:13:03 +0300

    add logger

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,7 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { CompanyService } from './company.service';
 
 @Controller('company')
 export class CompanyController {
+  private logger = new Logger(CompanyController.name);
+
   constructor(private readonly companyService: CompanyService) {}
 }
--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from 'src/common/services/prisma.service';
 import { Company } from './company.types';
 
 @Injectable()
 export class CompanyService {
+  private logger = new Logger(CompanyService.name);
+
   constructor(private readonly prisma: PrismaService) {}
 
   public async getCompanyById(id: number): Promise<Company | null> {
