6cb61d8e3bd9150740a650658bf79b9c689496e4
Denis Belyaev <d.belyaev@nwamo.ru>
2023-02-18 13:13:14 +0300

    add logger

--- src/estate/estate.controller.ts
+++ src/estate/estate.controller.ts
@@ -1,7 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { EstateService } from './estate.service';
 
 @Controller('estates')
 export class EstateController {
+  private logger = new Logger(EstateController.name);
+
   constructor(private readonly estateService: EstateService) {}
 }
