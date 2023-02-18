49aee0b5bb0feac2eeeeb41d0d5e8c16f5324886
Denis Belyaev <d.belyaev@nwamo.ru>
2023-02-18 13:21:57 +0300

    upd readme

--- src/counterparty/counterparty.controller.ts
+++ src/counterparty/counterparty.controller.ts
@@ -1,7 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { CounterpartyService } from './counterparty.service';
 
 @Controller('counterparty')
 export class CounterpartyController {
+  private logger = new Logger(CounterpartyController.name);
+
   constructor(private readonly counterpartyService: CounterpartyService) {}
 }
