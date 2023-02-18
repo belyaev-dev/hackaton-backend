457784ecaf818c7cb0afc5adcc4b1153bbe45a76
Denis Belyaev <d.belyaev@nwamo.ru>
2023-02-18 13:13:09 +0300

    add counterparty enum

--- src/counterparty/counterparty.types.ts
+++ src/counterparty/counterparty.types.ts
@@ -1,3 +1,10 @@
 import type { Counterparty as PrismaCounterparty } from '@prisma/client';
 
 export type Counterparty = PrismaCounterparty;
+
+export enum CounterpartyType {
+  IndividualEntrepreneur = 'INDIVIDUAL_ENTREPRENEUR',
+  LimitedLiabilityCompany = 'LIMITED_LIABILITY_COMPANY',
+  JointStockCompany = 'JOINT_STOCK_COMPANY',
+  Other = 'OTHER',
+}
