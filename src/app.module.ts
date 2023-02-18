dc7ee698dce632ef4fdf417c26687f2ae42def26
Denis Belyaev <d.belyaev@nwamo.ru>
2023-02-18 06:48:07 +0300

    update imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CompanyModule } from './company/company.module';
+import { ObjectModule } from './object/object.module';
+import { CounterpartyModule } from './counterparty/counterparty.module';
+import { ReservationModule } from './reservation/reservation.module';
+import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    CompanyModule,
+    ObjectModule,
+    CounterpartyModule,
+    ReservationModule,
+    TestModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
